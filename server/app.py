63c5bab9306edd9f06781779514ee97059902253
Tushar <upscadviser@gmail.com>
2026-06-25 15:07:31 +0530

    chore: setup virtual environment dependencies and configure database path

--- server/app.py
+++ server/app.py
@@ -1,13 +1,15 @@
-#!/usr/bin/env python3
-
+import os
 from flask import Flask, make_response, jsonify, session
 from flask_migrate import Migrate
 
 from models import db, Article, User, ArticleSchema, UserSchema
 
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DATABASE_PATH = os.path.join(BASE_DIR, "instance", "app.db")
+
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DATABASE_PATH}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
@@ -27,7 +29,23 @@ def index_articles():
 
 @app.route('/articles/<int:id>')
 def show_article(id):
-    pass
+    # Initialize the page_views count if not present
+    if 'page_views' not in session:
+        session['page_views'] = 0
+    
+    # Increment the page_views count by 1
+    session['page_views'] += 1
+
+    # Check if pageview count exceeds 3
+    if session['page_views'] > 3:
+        return make_response(jsonify({'message': 'Maximum pageview limit reached'}), 401)
+
+    # Retrieve the article
+    article = db.session.get(Article, id)
+    if not article:
+        return make_response(jsonify({'error': 'Article not found'}), 404)
+        
+    return make_response(jsonify(ArticleSchema().dump(article)), 200)
 
 
 if __name__ == '__main__':
