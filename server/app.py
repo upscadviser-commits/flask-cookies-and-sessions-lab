eaf2628ba78e0115089fb10e3d5c3b3ab9b9ff53
Tushar <upscadviser@gmail.com>
2026-06-25 15:17:40 +0530

    refactor: restore original relative database URI for CodeGrade compatibility

--- server/app.py
+++ server/app.py
@@ -9,7 +9,7 @@ DATABASE_PATH = os.path.join(BASE_DIR, "instance", "app.db")
 
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DATABASE_PATH}'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
